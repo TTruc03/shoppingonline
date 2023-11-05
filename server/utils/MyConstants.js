62172d43f6cdf834c3400ba26838d1598d18022d
TTruc03 <127476378+TTruc03@users.noreply.github.com>
2023-11-05 13:50:25 +0700

    first

---
+++ server/utils/MyConstants.js
@@ -0,0 +1,11 @@
+const MyConstants = {
+    DB_SERVER: 'cluster0.5megdkh.mongodb.net',
+    DB_USER: 'thanhtruc2023',
+    DB_PASS: 'Susu18022018',
+    DB_DATABASE: 'shoppingonline',
+    JWT_SECRET: '0334746667',
+    JWT_EXPIRES: '36000000', // in milliseconds
+    EMAIL_USER: 'huynhtruc21052003@gmail.com', // gmail service
+    EMAIL_PASS: 'mlzzzqjqteontgzu'
+  };
+  module.exports = MyConstants;
